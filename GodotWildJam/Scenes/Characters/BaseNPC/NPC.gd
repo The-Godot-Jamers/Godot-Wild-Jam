497735d2b8c383598bb94a5c14e370ae95e5e7b4
Zon Petilla <Burningwoodmedia@gmail.com>
2018-09-16 04:53:33 +0900

    Need more time, still working on characters

--- GodotWildJam/Scenes/Characters/BaseNPC/NPC.gd
+++ GodotWildJam/Scenes/Characters/BaseNPC/NPC.gd
@@ -9,21 +9,37 @@ signal knocked_down #knock down status and animation
 
 export (PackedScene) var Bullet # for Thrown Obejcts
 export (int) var speed 
-export (float) var rotation_speed #for character rotation 
+export (float) var rotation_speed #for character rotation
+export (int) var health
 
+var velocity = Vector2()
+var can_throw = true
+var alive = true #Death animation
+var knocked_down = false #Knocked down animation
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+
 	pass
 
-#func _process(delta):
+func control(delta):
+	
+	pass
+
+func _physics_process(delta):
+	if not alive:
+		return
+#	control(delta)
+	move_and_slide(velocity)
+	if knocked_down:
+		return
+	control(delta)
+#	move_and_slide(velocity)
+	 
+	
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	pass
-
-
+#pass
--- GodotWildJam/Scenes/Characters/Player/Player.gd
+++ GodotWildJam/Scenes/Characters/Player/Player.gd
@@ -1,13 +1,33 @@
 extends "res://Scenes/Characters/BaseNPC/NPC.gd"
 
+func control(delta):
+	
+	#$Player/AnimatedSprite.play("walk")
+	$Head.look_at(get_global_mouse_position())
+	var rot_dir =0
+	if Input.is_action_pressed('Right'):
+		rot_dir += 1
+	if Input.is_action_pressed('Left'):
+		rot_dir -= 1
+	rotation += rotation_speed *rot_dir *delta
+	if Input.is_action_pressed('Forward'):
+		$Head/AnimatedSprite.play("walk")
+		velocity=Vector2(speed,0).rotated(rotation)
+		
+	if Input.is_action_pressed('Back'):
+		velocity=Vector2(-speed/2,0).rotated(rotation)  
+
+
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
-func _ready():
+#func _ready():
+	#$Player/AnimatedSprite.play("walk")
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+#	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
