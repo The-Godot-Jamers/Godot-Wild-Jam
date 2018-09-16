749c548b5e6edc709491dfc02968d3f5cbf55a51
Zon Petilla <Burningwoodmedia@gmail.com>
2018-09-17 01:38:46 +0900

    Changes Made
    Added NPC Art
    Added gds for police and rioters
    Fixed player

--- GodotWildJam/Scenes/Characters/Player/Player.gd
+++ GodotWildJam/Scenes/Characters/Player/Player.gd
@@ -1,25 +1,32 @@
 extends "res://Scenes/Characters/BaseNPC/NPC.gd"
 
+
 func control(delta):
 	
 	#$Player/AnimatedSprite.play("walk")
 	$Head.look_at(get_global_mouse_position())
-	var rot_dir =0
+	$Body.look_at(get_global_mouse_position())
+	var m_speed = speed*10
+
 	if Input.is_action_pressed('Right'):
-		rot_dir += 1
+		velocity = Vector2(m_speed,0)
 	if Input.is_action_pressed('Left'):
-		rot_dir -= 1
-	rotation += rotation_speed *rot_dir *delta
-	if Input.is_action_pressed('Forward'):
-		$Head/AnimatedSprite.play("walk")
-		velocity=Vector2(speed,0).rotated(rotation)
+		velocity = Vector2(-m_speed,0)
+	if Input.is_action_pressed('Up'):
+		velocity = Vector2(0,m_speed)
+	if Input.is_action_pressed('Down'):
+		velocity = Vector2(0,-m_speed) 
+	if velocity ==  Vector2(0,0): 
+		$Body.play("idle")
+	else:
+		move_and_slide (velocity,Vector2(0,0))
+		$Body.play("walk")
+	velocity = Vector2(0,0)
+
+func _input(event):
+	
+	if Input.is_action_just_pressed('ui_accept'):
 		
-	if Input.is_action_pressed('Back'):
-		velocity=Vector2(-speed/2,0).rotated(rotation)  
-
-
-# class member variables go here, for example:
-# var a = 2
 # var b = "textvar"
 
 #func _ready():
---
+++ GodotWildJam/Scenes/Characters/Police/Police.gd
@@ -0,0 +1,8 @@
+extends "res://Scenes/Characters/BaseNPC/NPC.gd"
+
+#They should have special base behaviors as a class.
+
+func _ready():
+
+	pass
+
