d05c48fef5acf6a58f2567fd6a690c12dcf8992c
Unknown <kirvesmies70@hotmail.com>
2018-09-16 11:40:54 +0300

    camera zoom fixes

--- GodotWildJam/Scenes/camera/PlayerCamera.gd
+++ GodotWildJam/Scenes/camera/PlayerCamera.gd
@@ -1,13 +1,14 @@
 extends Camera2D
 
-export var zoom_amt = Vector2(0.05, 0.05)
+export var zoom_amt = Vector2(0.5, 0.5)
 export var zoom_time = 0.2
 
 func _input(event):
-	if Input.is_action_just_pressed("zoom_out"):
-		$Tween.interpolate_property(self,"zoom",zoom,zoom + zoom_amt, zoom_time,Tween.TRANS_QUAD,Tween.EASE_OUT_IN)
-		$Tween.start()
-	if Input.is_action_just_pressed("zoom_in"):
-		$Tween.interpolate_property(self,"zoom",zoom,zoom - zoom_amt, zoom_time,Tween.TRANS_QUAD,Tween.EASE_OUT_IN)
-		$Tween.start()
+	if not $Tween.is_processing():
+		if Input.is_action_just_pressed("zoom_out"):
+			$Tween.interpolate_property(self,"zoom",zoom,zoom + zoom_amt, zoom_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+			$Tween.start()
+		if Input.is_action_just_pressed("zoom_in"):
+			$Tween.interpolate_property(self,"zoom",zoom,zoom - zoom_amt, zoom_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+			$Tween.start()
 	
