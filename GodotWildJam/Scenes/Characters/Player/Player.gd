b3572547297859fea464eff2bce95c85512eafc3
Unknown <kirvesmies70@hotmail.com>
2018-09-17 00:09:27 +0300

    player deserves his own script

--- GodotWildJam/Scenes/Characters/Player/Player.gd
+++ GodotWildJam/Scenes/Characters/Player/Player.gd
@@ -1,5 +1,35 @@
-extends "res://Scenes/Characters/BaseNPC/NPC.gd"
+extends KinematicBody2D #player earns its place as independent of NPCs
 
+signal health_changed
+signal dead #for death status and death animations
+signal knocked_down #knock down status and animation
+
+#baselines for everyone unless chanced.
+var hot_headedness = 0  #How likely to start acting violent
+var care_about_issue = 50 #How much do they care about this particular issue
+var police_interest = 0 #how much do you annoy the police
+
+export (PackedScene) var Bullet # for Thrown Obejcts
+export (int) var speed 
+export (float) var rotation_speed #for character rotation
+export (int) var health
+
+var velocity = Vector2()
+var can_throw = true
+var alive = true #Death animation
+var knocked_down = false #Knocked down animation
+
+func _ready():
+	#player setup
+	pass
+
+func _physics_process(delta):
+	if not alive:
+		return
+	move_and_slide(velocity)
+	if knocked_down:
+		return
+	control(delta)
 
 func control(delta):
 	
@@ -24,7 +54,6 @@ func control(delta):
 	velocity = Vector2(0, 0)
 
 func _input(event):
-	
 	if Input.is_action_just_pressed('ui_accept'):
 		Globals.riot_lvl += 1
 		Globals.police_lvl += 1
